dbab8e50d33a163af2af40b84bc0bc9b157c207b
HunarPG <HunarSangwanPG@gmail.com>
2023-02-28 17:27:29 +0530

    Implemented Sierra's DNS

--- src/data/field_effects/field_effect_objects.h
+++ src/data/field_effects/field_effect_objects.h
@@ -267,7 +267,7 @@ static const union AnimCmd *const sAnimTable_Arrow[] =
 
 const struct SpriteTemplate gFieldEffectObjectTemplate_Arrow = {
     .tileTag = TAG_NONE,
-    .paletteTag = TAG_NONE,
+    .paletteTag = FLDEFF_PAL_TAG_ARROW,
     .oam = &gObjectEventBaseOam_16x16,
     .anims = sAnimTable_Arrow,
     .images = sPicTable_Arrow,
@@ -1287,6 +1287,7 @@ const struct SpriteTemplate gFieldEffectObjectTemplate_Rayquaza = {
 };
 
 static const struct SpritePalette sSpritePalette_Unused = {gObjectEventPal_Npc3, FLDEFF_PAL_TAG_UNKNOWN};
+const struct SpritePalette gSpritePalette_ArrowEmotionsFieldEffect = {gObjectEventPal_Brendan, FLDEFF_PAL_TAG_ARROW};
 
 static const struct SpriteFrameImage sPicTable_RockClimbBlob[] = {
     overworld_frame(gFieldEffectObjectPic_RockClimbBlob, 4, 4, 1),
