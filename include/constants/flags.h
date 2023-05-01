00141efda31593f5f8e8df0c5b18c4ee3cc8d2cc
HunarPG <HunarSangwanPG@gmail.com>
2023-05-01 11:43:32 +0530

    Added Rival in Rival's House 2F

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -41,8 +41,8 @@
 #define FLAG_TEMP_1F     (TEMP_FLAGS_START + 0x1F)
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 
-#define FLAG_MOM_SAY_DONT_GO_IN_GRASS    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
+#define FLAG_MOM_SAY_DONT_GO_IN_GRASS    0x20 
+#define Remove_Rival_From_His_House    0x21
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
