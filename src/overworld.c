9d79a251e243f2fd85c39090e1d54a79a6c61bef
HunarPG <HunarSangwanPG@gmail.com>
2023-08-05 10:47:35 +0530

    Revert "Made HM not required to teach(https://github.com/pret/pokeemerald/wiki/Use-HMs-Without-Any-Pokemon-in-your-Party-Knowing-Them)"
    This reverts commit 74aaabf530cd788665b2df72cb1e6ef06a6bc0f3.

--- src/overworld.c
+++ src/overworld.c
@@ -66,8 +66,6 @@
 #include "constants/songs.h"
 #include "constants/trainer_hill.h"
 #include "constants/weather.h"
-#include "item.h"
-#include "constants/items.h"
 
 struct CableClubPlayer
 {
@@ -175,7 +173,6 @@ static void TransitionMapMusic(void);
 static u8 GetAdjustedInitialTransitionFlags(struct InitialPlayerAvatarState *, u16, u8);
 static u8 GetAdjustedInitialDirection(struct InitialPlayerAvatarState *, u8, u16, u8);
 static u16 GetCenterScreenMetatileBehavior(void);
-static bool8 CanLearnFlashInParty(void);
 
 static void *sUnusedOverworldCallback;
 static u8 sPlayerLinkStates[MAX_LINK_PLAYERS];
@@ -3261,17 +3258,4 @@ static void SpriteCB_LinkPlayer(struct Sprite *sprite)
         sprite->invisible = ((sprite->data[7] & 4) >> 2);
         sprite->data[7]++;
     }
-}
-
-static bool8 CanLearnFlashInParty(void)
-{
-    u8 i;
-    for (i = 0; i < PARTY_SIZE; i++)
-    {
-        if (!GetMonData(&gPlayerParty[i], MON_DATA_SPECIES, NULL))
-            break;
-        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG) && CanLearnTeachableMove(GetMonData(&gPlayerParty[i]), ITEM_HM05 - ITEM_TM01))
-            return TRUE;
-    }
-    return FALSE;
 }
