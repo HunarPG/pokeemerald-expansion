b2b9ced6324a435e54e1e10feb0711fddd2c66a2
HunarPG <92902941+HunarPG@users.noreply.github.com>
2024-01-20 16:00:53 +0530

    Started Scripting Lake Verity Event

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -29,8 +29,8 @@
 // Overworld flags
 // To use the following features in scripting, replace the 0s with the flag ID you're assigning it to.
 // Eg: Replace with FLAG_UNUSED_0x264 so you can use that flag to toggle the feature.
-#define OW_FLAG_NO_ENCOUNTER        0x020  // If this flag is set, wild encounters will be disabled.
-#define OW_FLAG_NO_TRAINER_SEE      0x021  // If this flag is set, trainers will not battle the player unless they're talked to.
+#define OW_FLAG_NO_ENCOUNTER        0x022  // If this flag is set, wild encounters will be disabled.
+#define OW_FLAG_NO_TRAINER_SEE      0x023  // If this flag is set, trainers will not battle the player unless they're talked to.
 #define OW_FLAG_NO_COLLISION        0x025  // If this flag is set, the player will be able to walk over tiles with collision. Mainly intended for debugging purposes.
 
 #define BATTLE_PYRAMID_RANDOM_ENCOUNTERS    FALSE    // If set to TRUE, battle pyramid Pokemon will be generated randomly based on the round's challenge instead of hardcoded in src/data/battle_frontier/battle_pyramid_level_50_wild_mons.h (or open_level_wild_mons.h)
--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,11 +43,11 @@
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
 #define FLAG_MOM_SAY_DONT_GO_IN_GRASS    0x20 
-#define Remove_Rival_From_His_House    0x21
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define Remove_Rival_From_His_House      0x21
+#define FLAG_UNUSED_0x022    0x22
+#define FLAG_UNUSED_0x023    0x23
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_UNUSED_0x025    0x25
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
