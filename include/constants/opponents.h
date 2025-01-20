f8a22746f966d6aa89518f60aaf63f0a4d80e2ed
HunarPG <92902941+HunarPG@users.noreply.github.com>
2025-01-20 14:43:28 +0530

    Added scripts and signes to Route 203(Didn't do  Barry script yet)

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -8,10 +8,10 @@
 #define TRAINER_LOGAN                         2
 #define TRAINER_NATALIE                       3
 #define TRAINER_TYLER                         4
-#define TRAINER_GRUNT_AQUA_HIDEOUT_4          5
-#define TRAINER_GRUNT_SEAFLOOR_CAVERN_1       6
+#define TRAINER_MICHAEL                       5
+#define TRAINER_DALLAS                        6
 #define TRAINER_GRUNT_SEAFLOOR_CAVERN_2       7
-#define TRAINER_GRUNT_SEAFLOOR_CAVERN_3       8
+#define TRAINER_KAITLIN                       8
 #define TRAINER_GABRIELLE_1                   9
 #define TRAINER_GRUNT_PETALBURG_WOODS        10
 #define TRAINER_MARCEL                       11
