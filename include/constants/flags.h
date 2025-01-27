ef51cfce53183ba6ddabf1dac473922027957fa0
HunarPG <HunarSangwanPG@gmail.com>
2025-01-27 15:13:01 +0530

    Added dummy joker 1, 2 and their flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,14 +43,14 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_POKEMONPCMENU              0x20 // Unused Flag
-#define FLAG_GOT_COUPON_3               0x21 // Unused Flag
-#define FLAG_GOT_POKETCH                0x22 // Unused Flag
-#define FLAG_UNUSED_0x023               0x23 // Unused Flag
-#define FLAG_UNUSED_0x024               0x24 // Unused Flag
-#define FLAG_UNUSED_0x025               0x25 // Unused Flag
-#define FLAG_UNUSED_0x026               0x26 // Unused Flag
-#define FLAG_UNUSED_0x027               0x27 // Unused Flag
+#define FLAG_POKEMONPCMENU              0x20
+#define FLAG_GOT_COUPON_1               0x21
+#define FLAG_GOT_COUPON_2               0x22
+#define FLAG_GOT_COUPON_3               0x23
+#define FLAG_HIDE_JOKER_1               0x24
+#define FLAG_HIDE_JOKER_2               0x25
+#define FLAG_HIDE_JOKER_3               0x26
+#define FLAG_GOT_POKETCH                0x27
 #define FLAG_UNUSED_0x028               0x28 // Unused Flag
 #define FLAG_UNUSED_0x029               0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A               0x2A // Unused Flag
