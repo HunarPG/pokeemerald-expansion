8dcac25cf042eebc4fc42f316a3ee270e3302fd9
HunarPG <Hunarsangwanpg@gmail.com>
2026-03-18 21:28:09 +0530

    scripted sandgem town

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -63,7 +63,7 @@
 #define FLAG_HIDE_CYRUS_IN_LAKE                         0x29
 #define FLAG_HIDE_BARRY_IN_LAKE                         0x2A
 #define FLAG_ITEM_LAKE_VERITY_TM_FIRE_BLAST             0x2B
-#define FLAG_UNUSED_0x02C                               0x2C // Unused Flag
+#define FLAG_HIDE_RIVAL_IN_SANDGEM                      0x2C
 #define FLAG_UNUSED_0x02D                               0x2D // Unused Flag
 #define FLAG_UNUSED_0x02E                               0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F                               0x2F // Unused Flag
