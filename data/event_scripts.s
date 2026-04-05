3cc9dfe15b695155de21a7fd2066a2f9ed00cf00
HunarPG <Hunarsangwanpg@gmail.com>
2026-04-05 10:28:32 +0530

    Mom heal fully working

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1081,7 +1081,8 @@ EventScript_AfterWhiteOutHealMsg::
 EventScript_AfterWhiteOutMomHeal::
 	lockall
 	textcolor NPC_TEXT_COLOR_FEMALE
-	applymovement LOCALID_PLAYERS_HOUSE_1F_MOM, Common_Movement_WalkInPlaceFasterDown
+	applymovement LOCALID_MOM_SITTING, Common_Movement_WalkInPlaceFasterRight
+	applymovement OBJ_EVENT_ID_PLAYER, Common_Movement_WalkInPlaceFasterLeft
 	waitmovement 0
 	msgbox gText_HadQuiteAnExperienceTakeRest
 	call Common_EventScript_OutOfCenterPartyHeal
@@ -1425,24 +1426,25 @@ gText_MonsHealed::
 gText_HadQuiteAnExperienceTakeRest::
 	.string "MOM: {PLAYER}!\n"
 	.string "Welcome home.\p"
-	.string "It sounds like you had quite\n"
-	.string "an experience.\p"
+	.string "It sounds like you had quite the\n"
+	.string "experience out there.\p"
 	.string "Maybe you should take a quick\n"
-	.string "rest.$"
+	.string "rest so you can relax.$"
 
 gText_MomExplainHPGetPotions::
 	.string "MOM: Oh, good! You and your\n"
 	.string "POKéMON are looking great.\p"
-	.string "I just heard from {STR_VAR_1}.\p"
-	.string "He said that POKéMON's energy is\n"
-	.string "measured in HP.\p"
+	.string "Oh, yes, I heard from Professor.\n"
+	.string "Rowan.\p"
+	.string "He said that a POKéMON's energy is\n"
+	.string "measured in HP (Hit Points).\p"
 	.string "If your POKéMON lose their HP,\n"
-	.string "you can restore them at any\l"
+	.string "you can restore it at any\l"
 	.string "POKéMON CENTER.\p"
 	.string "If you're going to travel far away,\n"
 	.string "the smart TRAINER stocks up on\l"
 	.string "POTIONS at the POKéMON MART.\p"
-	.string "Make me proud, honey!\p"
+	.string "Make me proud, honey!\n"
 	.string "Take care!$"
 
 gText_RegisteredTrainerinPokeNav::
