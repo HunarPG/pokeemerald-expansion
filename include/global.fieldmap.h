42d9286e3d3a4afe854fb0906daf77dfdc1ed27c
HunarPG <Hunarsangwanpg@gmail.com>
2026-06-15 22:29:42 +0530

    fixed the game

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -235,7 +235,7 @@ struct MapHeader
     /* 0x15 */ u8 weather;
     /* 0x16 */ u8 mapType;
     /* 0x17 */ s8 floorNumber;
-    /* 0x18 */ u16 nightMusic;
+    /* 0x19 */ u16 nightMusic;
                // fields correspond to the arguments in the map_header_flags macro
     /* 0x1A */ bool8 allowCycling:1;
                bool8 allowEscaping:1; // Escape Rope and Dig
